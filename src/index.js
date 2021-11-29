a466c0ce057886ba51eff7a77fbf3563900e7595
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2021-11-29 17:33:15 +0300

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-alert("Hello world")
+let now = new Date();
+
+let date = now.getDate();
+let year = now.getFullYear();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let month = months[now.getMonth()];
+let hour = now.getHours();
+let min = now.getMinutes();
+let today = document.querySelector("p");
+today.innerHTML = `${day} ${date} ${month} ${hour}:${min} ${year}`;
+
+function search(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-text-input");
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = searchInput.value
+  }
+
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit",search)
+  
